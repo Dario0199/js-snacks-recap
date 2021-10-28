2bb9200960d5a4edcad8970c72b55bbce1428bee
Dario0122 <dario.di.cuia@gmail.com>
2021-10-28 17:48:15 +0200

    jsnack1 pomeriggio

---
+++ js-snacks-blocco-2/jsnack2/main.js
@@ -0,0 +1,20 @@
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+const nomi = [
+    'Giovanni',
+    'Luca',
+    'Francesco',
+    'Federico',
+    'Giuseppe'
+]
+
+const cognomi = [
+    'Rossi',
+    'Ferrari',
+    'Conti',
+    'Bianchi',
+    'Rizzo'
+]
+
+
+    
---
+++ js-snacks-blocco-2/jsnack3/main.js
@@ -0,0 +1,9 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+const numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
+
+let som = 0;
+
+for(let i = 0; i < 10; i++){
+    
+}
