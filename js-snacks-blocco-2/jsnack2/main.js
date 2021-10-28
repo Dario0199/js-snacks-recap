15f0654f291cbb5e928f7d9c8bc5a7725f0d376e
Dario0122 <dario.di.cuia@gmail.com>
2021-10-28 20:13:20 +0200

    blocco2 jsnack2

--- js-snacks-blocco-2/jsnack2/main.js
+++ js-snacks-blocco-2/jsnack2/main.js
@@ -1,4 +1,6 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+let numeroNome
+let numeroCognome
 
 const nomi = [
     'Giovanni',
@@ -16,5 +18,16 @@ const cognomi = [
     'Rizzo'
 ]
 
+let invitato1 
+let invitato2 
+let invitato3 
 
-    
+
+numeroNome = Math.floor(Math.random() *5)
+numeroCognome = Math.floor(Math.random() *5)
+
+invitato1 = nomi[numeroNome] + cognomi[numeroCognome];
+invitato2 = nomi[numeroNome] + cognomi[numeroCognome];
+invitato3 = nomi[numeroNome] + cognomi[numeroCognome]
+
+console.log(invitato1, invitato2, invitato3) ;
