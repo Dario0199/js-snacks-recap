466151abaa817251f57984f0b6a74132f03005c4
Dario0122 <dario.di.cuia@gmail.com>
2021-10-28 15:01:04 +0200

    blocco1 jsnack3

---
+++ js-snacks-blocco-1/jsnack3/main.js
@@ -0,0 +1,30 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array
+
+const dispari = []
+
+
+// for(let i = 0; i < 6; i++){
+     
+//     const numero = parseInt( prompt('Inserisci un numero') );
+
+//     if(numero % 2 !== 0){
+//         dispari.push(numero)
+//     }
+// }
+// console.log(dispari);
+
+let count = 0;
+
+while(count < 6){
+    
+    const numero = parseInt( prompt('Inserisci un numero') );
+
+    if(numero % 2 !== 0){
+        dispari.push(numero)
+    }
+
+    count++;
+}
+console.log(dispari);
